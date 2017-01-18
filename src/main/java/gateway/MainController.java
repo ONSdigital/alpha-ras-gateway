b3f4dbd7f0702d8e3ca06a6d9ede6c753317e799
Charles <enquiries@movingdigi.com>
2017-01-18 17:47:16 +0000

    extra change

--- src/main/java/gateway/MainController.java
+++ src/main/java/gateway/MainController.java
@@ -5,7 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.web.bind.annotation.PathVariable;
+import java.util.List;
+
 
 @RestController
 public class MainController {
@@ -24,6 +28,13 @@ public class MainController {
     String greeting = this.restTemplate.getForObject("http://say-hello/greeting", String.class);
 
     at this url : https://spring.io/guides/gs/client-side-load-balancing/ */
+
+    
+    @RequestMapping("/service-instances/{applicationName}")
+    public List<ServiceInstance> serviceInstancesByApplicationName(
+            @PathVariable String applicationName) {
+        return this.discoveryClient.getInstances(applicationName);
+    }
 
 }
 
