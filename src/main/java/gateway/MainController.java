7676373b9ead55ba5066bda1b3c594f456f7d963
Charles <enquiries@movingdigi.com>
2017-01-20 14:26:14 +0000

    configuring config server

--- src/main/java/gateway/MainController.java
+++ src/main/java/gateway/MainController.java
@@ -4,6 +4,7 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.beans.factory.annotation.Autowired;
