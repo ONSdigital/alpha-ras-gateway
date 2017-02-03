d76dfa6ded72a14c5b3bf46ba4e541f11df62c7f
Charles <enquiries@movingdigi.com>
2017-02-03 07:42:27 +0000

    set up zuul forwarding, it works :)

--- src/main/java/gateway/Application.java
+++ src/main/java/gateway/Application.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
 
-//@EnableZuulProxy
+@EnableZuulProxy
 @SpringBootApplication
 public class Application {
 
