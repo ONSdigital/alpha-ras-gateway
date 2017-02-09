0ca1dd22d8d07b71a4a3e49a0793d96814fb88ac
Nicholas Herriot <Nicholas.Herriot@gmail.com>
2017-02-09 15:03:14 +0000

    Adding new properties to get Zuul working.

--- src/main/java/gateway/MainController.java
+++ src/main/java/gateway/MainController.java
@@ -21,7 +21,7 @@ public class MainController {
 
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "Greetings from Zuul!";
     }
 
     
